db98071b1df64c3117bd7bf4241d60aaed52ea4a
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-02-07 20:39:21 -0300

    Code coverage threshhold

--- vite.config.ts
+++ vite.config.ts
@@ -17,12 +17,19 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setupTests.ts'],
     coverage: {
+      provider: 'v8',
       exclude: [
         ...configDefaults.exclude,
         '*.config.*',
         'src/main.tsx',
         'src/vite-env.d.ts',
       ],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
     },
   },
 });
