3e093722de6cefc9aabd49ec320a676ede37e580
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-02-11 16:17:49 -0300

    Animate copy button

--- src/app/components/ui/buttons/CopyTextButton.tsx
+++ src/app/components/ui/buttons/CopyTextButton.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import { Button } from '@/app/components/ui/buttons/Button';
 import { ClipboardIcon } from '@heroicons/react/16/solid';
 
 function CopyTextButton({ text }: { text: string }) {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(text);
+    setIsActive(true);
+    setTimeout(() => setIsActive(false), 1000);
+  };
+
   return (
-    <Button
-      className={'h-[25px] p-1'}
-      onClick={() => navigator.clipboard.writeText(text)}
-    >
-      <ClipboardIcon />
+    <Button className="h-[25px] p-1" onClick={handleClick}>
+      <ClipboardIcon
+        className={`transition-colors duration-300 ${isActive ? 'text-blue-400' : ''
+          }`}
+      />
     </Button>
   );
 }
