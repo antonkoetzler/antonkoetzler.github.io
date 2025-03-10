537384637af6e5fd48f272271e0d2a81e7e03187
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-03-09 21:36:49 -0300

    add expo and remove node from tech stack

--- src/app/components/modules/main/components/MainScreenBodyTechStackSection.tsx
+++ src/app/components/modules/main/components/MainScreenBodyTechStackSection.tsx
@@ -11,7 +11,6 @@ function MainScreenBodyTechStackSection() {
         <MainScreenBodySectionImage src={ImageStore.flutter.src} alt={ImageStore.flutter.alt} />
         <MainScreenBodySectionImage src={ImageStore.javascript.src} alt={ImageStore.javascript.alt} />
         <MainScreenBodySectionImage src={ImageStore.typescript.src} alt={ImageStore.typescript.alt} />
-        <MainScreenBodySectionImage src={ImageStore.node.src} alt={ImageStore.node.alt} />
         <MainScreenBodySectionImage src={ImageStore.bun.src} alt={ImageStore.bun.alt} />
         <MainScreenBodySectionImage src={ImageStore.react.src} alt={ImageStore.react.alt} />
         <MainScreenBodySectionImage src={ImageStore.tailwind.src} alt={ImageStore.tailwind.alt} />
@@ -19,6 +18,7 @@ function MainScreenBodyTechStackSection() {
         <MainScreenBodySectionImage src={ImageStore.electron.src} alt={ImageStore.electron.alt} />
         <MainScreenBodySectionImage src={ImageStore.vite.src} alt={ImageStore.vite.alt} />
         <MainScreenBodySectionImage src={ImageStore.next.src} alt={ImageStore.next.alt} />
+        <MainScreenBodySectionImage src={ImageStore.expo.src} alt={ImageStore.expo.alt} />
         <MainScreenBodySectionImage src={ImageStore.php.src} alt={ImageStore.php.alt} />
         <MainScreenBodySectionImage src={ImageStore.laravel.src} alt={ImageStore.laravel.alt} />
         <MainScreenBodySectionImage src={ImageStore.sqlite.src} alt={ImageStore.sqlite.alt} />
