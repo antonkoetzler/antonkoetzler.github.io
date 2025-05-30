1dd52140c0468f419ae924eea019be3ac4234de2
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-05-30 13:24:25 -0300

    Remove unused import

--- src/test/components/modules/main/components/MainScreenBodyContactInformationSection.test.tsx
+++ src/test/components/modules/main/components/MainScreenBodyContactInformationSection.test.tsx
@@ -1,5 +1,4 @@
 import MainScreenBodyContactInformationSection from '@/app/components/modules/main/components/MainScreenBodyContactInformationSection';
-import i18n from '@/i18n';
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
